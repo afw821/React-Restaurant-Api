70abef293e2433e3453d894beff1397485af6325
afw821 <afw821@gmail.com>
2020-08-06 14:29:53 -0400

    fixed deployed api url for re-deployment

--- startup/db.js
+++ startup/db.js
@@ -5,12 +5,15 @@ require("dotenv").config();
 
 module.exports = async function () {
   const isDbProd = config.get("db_production");
+  console.log("is db prod", isDbProd);
   if (isDbProd) {
+    console.log("-----------------IFF-------------------");
     var connection = await mongoose.connect(
       `mongodb+srv://afw821:${process.env.DB_PASSWORD}@alexdb-tdxtv.mongodb.net/heroku_v5swwtl2?retryWrites=true&w=majority`,
       { useNewUrlParser: true }
     );
   } else {
+    console.log("-------------else--------------");
     var connection = await mongoose.connect(
       "mongodb://localhost/react-restaurant-API",
       { useNewUrlParser: true }
@@ -18,7 +21,7 @@ module.exports = async function () {
   }
 
   const AutoIncrement = AutoIncrementFactory(connection);
-
+  console.log("--------CONNECTION----------------", connection);
   if (connection) console.log("Connected to MongoDB://React-Restaurant-API");
   else console.log("Error connecting to MongoDB");
 };
