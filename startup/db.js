753b6b2b194cbaa79712c45a323f4f11477a6881
afw821 <afw821@gmail.com>
2020-08-06 14:59:03 -0400

    changed mongo uri

--- startup/db.js
+++ startup/db.js
@@ -6,12 +6,9 @@ require("dotenv").config();
 module.exports = async function () {
   const isDbProd = config.get("db_production");
   console.log("is db prod", isDbProd);
-  if (isDbProd) {
+  if (process.env.DB_URI) {
     console.log("-----------------IFF-------------------");
-    var connection = await mongoose.connect(
-      `mongodb+srv://afw821:${process.env.DB_PASSWORD}@alexdb-tdxtv.mongodb.net/heroku_v5swwtl2?retryWrites=true&w=majority`,
-      { useNewUrlParser: true }
-    );
+    var connection = await mongoose.connect(process.env.DB_URI);
   } else {
     console.log("-------------else--------------");
     var connection = await mongoose.connect(
